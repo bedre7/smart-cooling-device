121a24efb56f5264c67d61c8d35ead0b4755a1f7
Bedru Umer Mohammed <63902795+bedre7@users.noreply.github.com>
2022-05-01 16:37:47 +0300

    update

--- src/com/project/smartdevice/IUserService.java
+++ src/com/project/smartdevice/IUserService.java
@@ -2,6 +2,6 @@ package com.project.smartdevice;
 
 public interface IUserService {
     String readUserInput();
-    boolean searchUser(String username);
+    boolean searchuser(String username);
     User loginUser(String username, String password);
 }
--- src/com/project/smartdevice/SmartDeviceApp.java
+++ src/com/project/smartdevice/SmartDeviceApp.java
@@ -4,15 +4,5 @@ public class SmartDeviceApp {
     public static void main(String[] args)
     {
         NetworkInterface networkInterface = new NetworkInterface();
-//        networkInterface.displayMenu();
-
-        System.out.println("test for github connection");
-
-//        networkInterface.turnOnCooler();
-//        networkInterface.turnOffCooler();
-
-//        for(Icons icon : Icons.values())
-//            System.out.print(icon + " ");
-
     }
 }
