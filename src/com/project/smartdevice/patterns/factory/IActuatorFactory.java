1cb0f3a77d848e160fc2376370e712740dfb7473
SEIDY KANTE <82980518+SEIDY-KANTE@users.noreply.github.com>
2022-05-05 17:12:53 +0300

    Delete src/com/project/smartdevice/patterns/factory directory

--- src/com/project/smartdevice/patterns/factory/IActuatorFactory.java
+++
@@ -1,7 +0,0 @@
-package com.project.smartdevice.patterns.factory;
-
-import com.project.smartdevice.IActuator;
-
-public interface IActuatorFactory {
-    IActuator factoryMethod();
-}
--- src/com/project/smartdevice/patterns/factory/ITemperatureFactory.java
+++
@@ -1,7 +0,0 @@
-package com.project.smartdevice.patterns.factory;
-
-import com.project.smartdevice.ITemperatureSensor;
-
-public interface ITemperatureFactory {
-    ITemperatureSensor factoryMethod();
-}
