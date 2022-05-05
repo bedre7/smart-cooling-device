29c707318dc14a7498ef5dd74a67a4175edab15b
SEIDY-KANTE <82980518+SEIDY-KANTE@users.noreply.github.com>
2022-05-05 17:06:12 +0300

    optimization

--- src/com/project/smartdevice/SmartDeviceApp.java
+++ src/com/project/smartdevice/SmartDeviceApp.java
@@ -3,8 +3,6 @@ package com.project.smartdevice;
 public class SmartDeviceApp {
     public static void main(String[] args)
     {
-        final int x;
-        x = 10;
         ISmartDevice smartDevice = new SmartDevice();
         smartDevice.run();
     }
